d36f2f24cd39db0920c4cdee7e5b71536271e989
Shay Anderson <nexframe@yahoo.com>
2014-10-22 16:43:06 -0400

    mod bootstrap

--- _app/com/app.bootstrap.php
+++ _app/com/app.bootstrap.php
@@ -16,26 +16,26 @@
 // Logging
 //////////////////////////////////////////////////////////////////////////
 // set logging level
-logger()->setLogLevel(\Drone\Logger::LEVEL_TRACE);
-// logger()->setLogFile(PATH_ROOT . '_app/var/drone.log'); // set log file (optional)
+drone()->log->setLogLevel(\Drone\Logger::LEVEL_TRACE);
+// drone()->log->setLogFile(PATH_ROOT . '_app/var/drone.log'); // set log file (optional)
 
 
 //////////////////////////////////////////////////////////////////////////
 // Settings
 //////////////////////////////////////////////////////////////////////////
 // framework settings
-// set(\Drone\Core::KEY_DEBUG, false); // debug mode - off for production
-// set(\Drone\Core::KEY_ERROR_BACKTRACE, false); // backtrace in log - off for production
-// set(\Drone\Core::KEY_ERROR_LOG, true); // errors to server log - on for production
+// drone()->set(\Drone\Core::KEY_DEBUG, false); // debug mode - off for production
+// drone()->set(\Drone\Core::KEY_ERROR_BACKTRACE, false); // backtrace in log - off for production
+// drone()->set(\Drone\Core::KEY_ERROR_LOG, true); // errors to server log - on for production
 
 
 //////////////////////////////////////////////////////////////////////////
 // Error handlers
 //////////////////////////////////////////////////////////////////////////
-error(function($error) { pa('<div style="color:#f00;">' . $error,
+drone()->error(function($error) { pa('<div style="color:#f00;">' . $error,
 	debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS), '</div>'); });
-error(404, function() { drone()->run('error->\ErrorController->_404'); });
-error(500, function() { drone()->run('error->\ErrorController->_500'); });
+drone()->error(404, function() { drone()->run('error->\ErrorController->_404'); });
+drone()->error(500, function() { drone()->run('error->\ErrorController->_500'); });
 
 
 //////////////////////////////////////////////////////////////////////////
